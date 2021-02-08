f5c146982892c0a1f1fea57acef5ee343b32a6a6
Iulian Popa <bobel2007@outlook.com>
2021-02-08 13:31:54 +0200

    Add product model

---
+++ src/main/java/com/codecool/shop/model/Product.java
@@ -0,0 +1,52 @@
+package com.codecool.shop.model;
+
+public class Product {
+
+    private String title;
+    private String description;
+    private ProductCategory category;
+    private double price;
+
+    public Product() {
+
+    }
+
+    public Product(String title, String description, ProductCategory category, double price) {
+        this.title = title;
+        this.description = description;
+        this.category = category;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ProductCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(ProductCategory category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
---
+++ src/main/java/com/codecool/shop/model/ProductCategory.java
@@ -0,0 +1,8 @@
+package com.codecool.shop.model;
+
+public enum ProductCategory {
+    ELECTRONICS,
+    SPORTS,
+    HOUSEHOLD,
+    ESTATE
+}
