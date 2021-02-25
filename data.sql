317f2602a75a2194630aed400b3f889b00d01a65
Iulian Popa <bobel2007@outlook.com>
2021-02-25 11:42:59 +0200

    Add sql file

---
+++ data.sql
@@ -0,0 +1,15 @@
+INSERT INTO users
+    (id, email, first_name, last_name, password, phone)
+VALUES
+    (1, 'gigi@google.com', 'Gigel', 'Gigescu', 'dinti', '0722343655');
+
+INSERT INTO users
+    (id, email, first_name, last_name, password, phone)
+VALUES
+    (2, 'vali@google.com', 'Valentin', 'Vasilescu', 'test', '0722343633');
+
+
+INSERT INTO product
+    (id, category, description, image_link, price, title, user_id)
+VALUES
+    (333, 'SPORTS', 'Nou nouta, sunati pentru orice alte detalii', '', 19, 'Minge Adidas', 2);
